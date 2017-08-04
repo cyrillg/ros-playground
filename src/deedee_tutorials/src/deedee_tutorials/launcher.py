67e0d793c847306a17b7d1a03cbf7dc3278d8006
Cyrill Guillemot <cyrill.guillemot@gmail.com>
2017-08-04 22:56:45 +0800

    Begin writing the bringup node

---
+++ src/deedee_tutorials/src/deedee_tutorials/launcher.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import rospy
+import yaml
+import subprocess
+
+class Launcher:
+  ''' Node spawning the environment with respect to the global configs
+
+  '''
+  def __init__(self):
+
+
