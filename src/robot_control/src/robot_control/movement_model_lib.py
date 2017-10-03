5806ab5d3b168b767280eab98f9724e71ba84500
cyrillg <cyrill.guillemot@gmail.com>
2017-10-03 22:50:05 +0800

    Working version of the refactored movement_model

---
+++ src/robot_control/src/robot_control/movement_model_lib.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class DiffDrive:
+  def __init__(self, L, r):
+    # TODO: Should be read from configs
+    self.L = L
+    self.r = r
+
+  def transform(self, v, w):
+    ''' Transform linear and angular speed into wheel angular speeds
+    '''
+    u0 = (2*v + self.L*w) / (2*self.r)
+    u1 = (2*v - self.L*w) / (2*self.r)
+
+    return (u0, u1)
+
