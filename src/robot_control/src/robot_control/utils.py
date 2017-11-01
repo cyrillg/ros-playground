Fix Quaternion mistake in the utils.py pose conversion functions

src/robot_control/src/robot_control/utils.py
@@ -10,7 +10,7 @@ license: GNU GPL
 #!/usr/bin/env python
 
 from numpy import sqrt, arctan2, pi
-from geometry_msgs.msg import Pose, Pose2D
+from geometry_msgs.msg import Pose, Pose2D, Quaternion
 from tf.transformations import euler_from_quaternion as rpy_from_q
 from tf.transformations import quaternion_from_euler as q_from_rpy
 
@@ -30,7 +30,8 @@ def pose_to_pose2d(pose):
 
   ret_pose.x = pose.position.x
   ret_pose.y = pose.position.y
-  ret_pose.theta = rpy_from_q(pose.orientation)[2]
+  q = pose.orientation
+  ret_pose.theta = rpy_from_q(q[0], q[1], q[2], q[3])[2]
 
   return ret_pose
 
@@ -39,7 +40,11 @@ def pose2d_to_pose(pose):
 
   ret_pose.position.x = pose.x
   ret_pose.position.y = pose.y
-  ret_pose.orientation = q_from_rpy(0., 0., pose.theta)
+  q = q_from_rpy(0., 0., pose.theta)
+  ret_pose.orientation = Quaternion(x=q[0],
+                                    y=q[1],
+                                    z=q[2],
+                                    w=q[3])
 
   return ret_pose
 